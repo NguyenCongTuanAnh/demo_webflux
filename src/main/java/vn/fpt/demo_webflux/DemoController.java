939d61e34218903cbafce2c54170a7642ef0b22c
anhnct <anhnc17tb@gmail.com>
2024-08-24 11:59:00 +0700

    add demoController file

---
+++ src/main/java/vn/fpt/demo_webflux/DemoController.java
@@ -0,0 +1,12 @@
+package vn.fpt.demo_webflux;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping(value = "/")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}

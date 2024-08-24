82cee8187755c57201afe9be4bdac49f16f3734e
anhnct <anhnc17tb@gmail.com>
2024-08-24 15:44:47 +0700

    comment 2 endpoint

--- src/main/java/vn/fpt/demo_webflux/DemoController.java
+++ src/main/java/vn/fpt/demo_webflux/DemoController.java
@@ -9,12 +9,12 @@ public class DemoController {
     public String helloWorld() {
         return "Hello, World!";
     }
-      @GetMapping(value = "/demo")
-    public String demo() {
-        return "Hello, World2!";
-    }
-      @GetMapping(value = "/demo2")
-    public String demo2() {
-        return "Hello, World2!";
-    }
+//      @GetMapping(value = "/demo")
+//    public String demo() {
+//        return "Hello, World2!";
+//    }
+//      @GetMapping(value = "/demo2")
+//    public String demo2() {
+//        return "Hello, World2!";
+//    }
 }
